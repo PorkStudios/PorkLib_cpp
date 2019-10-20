822f6104f880f4d31f6b53ed22981628d4e6e3d5
DaMatrix <joey.rabil@gmail.com>
2019-10-20 12:28:12 +0200

    add some helpers for dealing with endianess and fields

--- source/porklib/types.h
+++ source/porklib/types.h
@@ -2,6 +2,18 @@
 #define PORKLIB_CPP_TYPES_H
 
 #include <cstdint>
+#include <sys/param.h>
+#include <byteswap.h>
+
+#ifndef BYTE_ORDER
+#error "BYTE_ORDER not defined"
+#endif
+
+#if BYTE_ORDER != BIG_ENDIAN
+#define IS_LITTLE_ENDIAN
+#else
+#define IS_BIG_ENDIAN
+#endif
 
 typedef std::size_t u_size;
 constexpr u_size SIZE_NULL = (1 << sizeof(u_size) << 3) - 1;
@@ -57,9 +69,177 @@ namespace porklib {
         template<typename A> A lsh1(A value) { return value << 1; }
 
         template<typename A> A identity(A value) { return value; }
-    }
-}
 
-#define IS_BIG_ENDIAN (*(uint16_t *)"\0\xff" < 0x100)
+        template<typename A> void free(A value) { delete value; }
+    }
+
+    struct u16_le   {
+        u16 v;
+        #ifdef IS_LITTLE_ENDIAN
+        operator u16 () { return this->v; }
+        u16& operator = (u16 v) { return this->v = v; }
+        #else
+        operator u16 () { return __bswap_16(this->v); }
+        u16& operator = (u16 v) { return this->v = __bswap_16(v); }
+        #endif
+
+        u16_le() {}
+        u16_le(u16 v) { *this = v; }
+    };
+
+    struct u32_le   {
+        u32 v;
+        #ifdef IS_LITTLE_ENDIAN
+        operator u32 () { return this->v; }
+        u32& operator = (u32 v) { return this->v = v; }
+        #else
+        operator u32 () { return __bswap_32(this->v); }
+        u32& operator = (u32 v) { return this->v = __bswap_32(v); }
+        #endif
+
+        u32_le() {}
+        u32_le(u32 v) { *this = v; }
+    };
+
+    struct u64_le   {
+        u64 v;
+        #ifdef IS_LITTLE_ENDIAN
+        operator u64 () { return this->v; }
+        u64& operator = (u64 v) { return this->v = v; }
+        #else
+        operator u64 () { return __bswap_64(this->v); }
+        u64& operator = (u64 v) { return this->v = __bswap_64(v); }
+        #endif
+
+        u64_le() {}
+        u64_le(u64 v) { *this = v; }
+    };
+
+    struct i16_le   {
+        i16 v;
+        #ifdef IS_LITTLE_ENDIAN
+        operator i16 () { return this->v; }
+        i16& operator = (i16 v) { return this->v = v; }
+        #else
+        operator i16 () { return __bswap_16(this->v); }
+        i16& operator = (i16 v) { return this->v = __bswap_16(v); }
+        #endif
+
+        i16_le() {}
+        i16_le(i16 v) { *this = v; }
+    };
+
+    struct i32_le   {
+        i32 v;
+        #ifdef IS_LITTLE_ENDIAN
+        operator i32 () { return this->v; }
+        i32& operator = (i32 v) { return this->v = v; }
+        #else
+        operator i32 () { return __bswap_32(this->v); }
+        i32& operator = (i32 v) { return this->v = __bswap_32(v); }
+        #endif
+
+        i32_le() {}
+        i32_le(i32 v) { *this = v; }
+    };
+
+    struct i64_le   {
+        i64 v;
+        #ifdef IS_LITTLE_ENDIAN
+        operator i64 () { return this->v; }
+        i64& operator = (i64 v) { return this->v = v; }
+        #else
+        operator i64 () { return __bswap_64(this->v); }
+        i64& operator = (i64 v) { return this->v = __bswap_64(v); }
+        #endif
+
+        i64_le() {}
+        i64_le(i64 v) { *this = v; }
+    };
+
+    struct u16_be   {
+        u16 v;
+        #ifdef IS_BIG_ENDIAN
+        operator u16 () { return this->v; }
+        u16& operator = (u16 v) { return this->v = v; }
+        #else
+        operator u16 () { return __bswap_16(this->v); }
+        u16& operator = (u16 v) { return this->v = __bswap_16(v); }
+        #endif
+
+        u16_be() {}
+        u16_be(u16 v) { *this = v; }
+    };
+
+    struct u32_be   {
+        u32 v;
+        #ifdef IS_BIG_ENDIAN
+        operator u32 () { return this->v; }
+        u32& operator = (u32 v) { return this->v = v; }
+        #else
+        operator u32 () { return __bswap_32(this->v); }
+        u32& operator = (u32 v) { return this->v = __bswap_32(v); }
+        #endif
+
+        u32_be() {}
+        u32_be(u32 v) { *this = v; }
+    };
+
+    struct u64_be   {
+        u64 v;
+        #ifdef IS_BIG_ENDIAN
+        operator u64 () { return this->v; }
+        u64& operator = (u64 v) { return this->v = v; }
+        #else
+        operator u64 () { return __bswap_64(this->v); }
+        u64& operator = (u64 v) { return this->v = __bswap_64(v); }
+        #endif
+
+        u64_be() {}
+        u64_be(u64 v) { *this = v; }
+    };
+
+    struct i16_be   {
+        i16 v;
+        #ifdef IS_BIG_ENDIAN
+        operator i16 () { return this->v; }
+        i16& operator = (i16 v) { return this->v = v; }
+        #else
+        operator i16 () { return __bswap_16(this->v); }
+        i16& operator = (i16 v) { return this->v = __bswap_16(v); }
+        #endif
+
+        i16_be() {}
+        i16_be(i16 v) { *this = v; }
+    };
+
+    struct i32_be   {
+        i32 v;
+        #ifdef IS_BIG_ENDIAN
+        operator i32 () { return this->v; }
+        i32& operator = (i32 v) { return this->v = v; }
+        #else
+        operator i32 () { return __bswap_32(this->v); }
+        i32& operator = (i32 v) { return this->v = __bswap_32(v); }
+        #endif
+
+        i32_be() {}
+        i32_be(i32 v) { *this = v; }
+    };
+
+    struct i64_be   {
+        i64 v;
+        #ifdef IS_BIG_ENDIAN
+        operator i64 () { return this->v; }
+        i64& operator = (i64 v) { return this->v = v; }
+        #else
+        operator i64 () { return __bswap_64(this->v); }
+        i64& operator = (i64 v) { return this->v = __bswap_64(v); }
+        #endif
+
+        i64_be() {}
+        i64_be(i64 v) { *this = v; }
+    };
+}
 
 #endif //PORKLIB_CPP_TYPES_H
